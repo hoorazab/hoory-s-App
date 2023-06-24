b770ec1a89c01174b65b468287baadba70f55bdb
hoorazab <azabhoor@gmail.com>
2023-06-24 14:20:19 +0400

    Initial Commit

---
+++ voice on/voice_onApp.swift
@@ -0,0 +1,17 @@
+//
+//  voice_onApp.swift
+//  voice on
+//
+//  Created by Hoor Azab on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct voice_onApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
